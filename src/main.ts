a6c42c6ee6cc60197458ae4f6d8b218473f25e8e
pengxincheng <pengxiaocheng180@163.com>
2023-04-17 17:10:49 +0800

    feat: 引入tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import router from './router';
 import store from './store';
+import './assets/tailwind.css';
 import App from './App.vue';
 
 const app = createApp(App);
