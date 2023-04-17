8bb2503446f17010e7dea3be4e2e195082eb65b1
pengxincheng <pengxiaocheng180@163.com>
2023-04-17 15:21:08 +0800

    feat: 增加vueuse，并自动按需引入

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
-import { ArcoResolver } from 'unplugin-vue-components/resolvers';
+import { ArcoResolver, VueUseComponentsResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,11 +11,14 @@ export default defineConfig({
     vue(),
     AutoImport({
       dts: './src/auto-imports.d.ts',
-      imports: ['vue', 'pinia', 'vue-router'],
+      imports: ['vue', 'pinia', 'vue-router', '@vueuse/core'],
       eslintrc: { enabled: true, filepath: './eslintrc-auto-import.json', globalsPropValue: true },
       resolvers: [ArcoResolver()],
     }),
-    Components({ dts: './src/components.d.ts', resolvers: [ArcoResolver({ sideEffect: true })] }),
+    Components({
+      dts: './src/components.d.ts',
+      resolvers: [ArcoResolver({ sideEffect: true }), VueUseComponentsResolver()],
+    }),
   ],
   resolve: {
     alias: {
