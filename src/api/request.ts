ffc6a70eb5081489d438d9352a17ca2b74416fec
pengxincheng <pengxiaocheng180@163.com>
2023-04-18 16:47:17 +0800

    feat: Axios简易封装

---
+++ src/api/request.ts
@@ -0,0 +1,59 @@
+import axios, { AxiosResponse, AxiosError, InternalAxiosRequestConfig } from 'axios';
+
+const BASE_URL_PREFIX = import.meta.env.VITE_API_BASEURL;
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL_PREFIX,
+  timeout: 1000 * 30,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 请求拦截器
+axiosInstance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  },
+);
+
+// 响应拦截器
+axiosInstance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    if (response.status === 200) {
+      return response.data;
+    }
+    return response;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  },
+);
+
+const service = {
+  get<T = any>(url: string, data?: object): Promise<T> {
+    return axiosInstance.get(url, { params: data });
+  },
+
+  post<T = any>(url: string, data?: object): Promise<T> {
+    return axiosInstance.post(url, data);
+  },
+
+  put<T = any>(url: string, data?: object): Promise<T> {
+    return axiosInstance.put(url, data);
+  },
+
+  delete<T = any>(url: string, data?: object): Promise<T> {
+    return axiosInstance.delete(url, data);
+  },
+
+  upload: (url: string, file: FormData | File) =>
+    axiosInstance.post(url, file, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    }),
+};
+
+export default service;
