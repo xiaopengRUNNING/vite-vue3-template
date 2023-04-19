cbb506c6bfa0f7d21ec73a326cc64ceb879e372d
pengxincheng <pengxiaocheng180@163.com>
2023-04-19 10:30:11 +0800

    feat: 增加页面标题配置，路由调整时修改页面标题

--- src/router/guard/index.ts
+++ src/router/guard/index.ts
@@ -7,6 +7,9 @@ Nprogress.configure({ showSpinner: false });
 
 function setupPageGuard(router: Router) {
   router.beforeEach(async (to) => {
+    document.title =
+      (to.meta.title as string) || import.meta.env.VITE_APP_TITLE;
+
     setRouteEmitter(to);
     if (!Nprogress.isStarted()) {
       Nprogress.start();
